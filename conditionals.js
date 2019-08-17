6a0e86dd3aa367bbed8c8864e1418119b1f0016b
Aiman <aimanaaw@gmail.com>
2019-08-17 07:13:33 +0000

    voting age

--- conditionals.js
+++ conditionals.js
@@ -1,23 +1,6 @@
-const raining = true;
-const cold = true;
+const isCitizen = true;
+const age = 26;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
-
-const temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
