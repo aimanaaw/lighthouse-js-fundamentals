fe5bd18defd7ea90ce20bfd011f5d50706b5c1f7
Aiman <aimanaaw@gmail.com>
2019-08-21 14:18:04 +0000

    Committing files and exercises

---
+++ factorials.js
@@ -0,0 +1,7 @@
+var solution = 1;
+
+for (var factorial = 12; factorial >=1; factorial -= 1) {
+  solution *= factorial;
+}
+
+console.log(solution);
---
+++ findMySeat.js
@@ -0,0 +1,5 @@
+for (var x = 0; x<=25; x+=1) {
+  for (var y = 0; y <= 99; y+=1) {
+    console.log(x + "-" + y);
+  }
+}
