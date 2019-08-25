669085fbce03c42bbb6a904914a66e09fc62636d
Aiman <aimanaaw@gmail.com>
2019-08-25 18:04:18 +0000

    committing the odd exercise

---
+++ odd.js
@@ -0,0 +1,21 @@
+// const isOdd = function (num) {
+//   if (num%2 === 0) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+const isOdd = function (num) {
+  return (num%2===1);
+}
+
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+
+
+
+// true if odd
+// false if even
